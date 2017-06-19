d9a4996e826db834b134d0657b3fdf15a3629190
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-06-18 22:46:36 -0700

    checkpoint - need to account for undefined/null object

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,40 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (Array.isArray(obj)) {
+    if(obj.length > 0) {
+      var string = '';
+      obj.forEach(function(x) {
+        string += (x).toString() + ',';
+      });
+      string = string.slice(0, string.length-1);
+      return `[${string}]`;
+    }
+    else {
+      return '[]';
+    }
+  }
+  if (typeof obj === 'object') {
+    if(Object.keys(obj).length > 0) {
+      var string = '';
+      for(var key in obj) {
+        if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+          string += key.toString() + ':' + obj[key].toString() + ',';
+        }
+      }
+      string = string.slice(0, string.length-1);
+      return `{${string}}`;
+    }
+    else {
+      return '{}';
+    }
+  }
+  else {
+    if (typeof obj === 'string') {
+      return `'${obj}'`;
+    }
+    else {
+      return '' + obj;
+    }
+  }
 };
